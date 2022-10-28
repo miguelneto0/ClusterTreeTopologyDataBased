07fcf80b9666290569b29a7a11c1ee7cc2571ee2
Miguel Neto <miguelneto0019@gmail.com>
2022-10-28 00:11:20 -0300

    Alteracao do codigo para identificar com coordenadas com clique

--- get_scenario_coordinates.py
+++ get_scenario_coordinates.py
@@ -3,18 +3,34 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import csv
 
+nodos = []
+
+# ler o arquivo de nodos
+with open('nodos_IEEE123testfeedr.txt', 'r') as f:
+    r = f.readlines()
+    print(r)
+
+# percorre cada linha e transforma em int cada ID para guardar na estrutura
+for a in r:
+    nodos.append(int(a))
+print(f'nodos total = {len(nodos)}')
+
 # inicializa o Arquivo em branco
 with open('IEEE123-coordinates.csv', 'w') as file:
     file.write("")
 
+count = -1
+
 # recupera as coordenados quando o click do botao for acionado
 def mouse_event(event):
+    global count
+    count += 1
     datax = round(event.xdata, 2)
     datay = round(event.ydata, 2)
     print(datax,' \t', datay)
     # abre o arquivo para escrever no modo append linha a linha
     with open('IEEE123-coordinates.csv', 'a') as file:
-        file.write(f'{datax},{datay}\n')
+        file.write(f'{nodos[count]}, {datax},{datay}\n')
 
 # ler a imagem do cenario passado
 img = mpimg.imread('C://Users//Miguel//Documents//ufsc 2022.2//Disciplina IoT//IEEE-123-node-test-feeder.png') 
